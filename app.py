0d215338469036dc9818adc602b869850b76005c
Berindeie Alexandru <alexandru.berindeie01@e-uvt.ro>
2023-10-30 18:13:01 +0200

    version 0.1.2 fixes for routes

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ import requests
 from datetime import datetime, timezone
 
 app = Flask(__name__)
+# allow routes for cross-origin requests
 CORS(app, resources={
     r"/create-reservation": {"origins": "http://localhost:3000"},
-    r"/get-reservation-data": {"origins": "http://localhost:3000"}
+    r"/get-reservation-data": {"origins": "http://localhost:3000"},
+    r"/update-scooter": {"origins": "http://localhost:3000"}
 })
 
 
+# connect db
 def get_db_connection():
     conn = sqlite3.connect('data/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 
+# create a new reservation
 @app.route("/create-reservation", methods=["POST"])
 def create_reservation():
     data = request.get_json()
@@ -30,7 +34,6 @@ def create_reservation():
     end_time = data.get("endTime")
     location = data.get("location")
 
-    # Convert start_time to a datetime object
     start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
 
     # Check if start_time is after the current time
@@ -55,25 +58,24 @@ def create_reservation():
     return jsonify({"message": "Reservation created"}), 201
 
 
+# send available scooters data for front
 @app.route("/get-reservation-data", methods=["GET"])
 def get_reservation_data():
     conn = sqlite3.connect('data/database.db')
     cursor = conn.cursor()
 
-    # Get the current time
     current_time = datetime.now(timezone.utc).strftime("YYYY-MM-DDTHH:MM:SSZ")
 
     # Fetch reservation data from the database with a filter
     cursor.execute(
         "SELECT first_name, last_name, phone_number, location "
         "FROM reservations "
-        "WHERE end_time <= ?", (current_time,)
+        "WHERE end_time <= ? AND available = 'yes'", (current_time,)
     )
     reservation_data = cursor.fetchall()
 
     conn.close()
 
-    # Convert the reservation data into a list of dictionaries
     reservation_list = [
         {
             "first_name": row[0],
@@ -87,17 +89,16 @@ def get_reservation_data():
     return jsonify(reservation_list)
 
 
+# make a scooter unavailable when taken
 @app.route("/update-scooter", methods=["POST"])
 def update_scooter_availability():
     data = request.get_json()
     first_name = data.get("first_name")
     last_name = data.get("last_name")
 
-    # Update scooter availability in your database
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Modify this query to update the scooter availability
     cursor.execute(
         "UPDATE reservations SET available = ? WHERE first_name = ? AND last_name = ?",
         ("no", first_name, last_name)
